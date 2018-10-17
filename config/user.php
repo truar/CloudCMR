a01e92ff257d1e66753c79281b2ebe8226c291d0
Thibault RUARO <truaro@MacBook-Pro-de-Thibault.local>
2018-10-17 14:47:22 +0200

    Change to override the value with .env file if present

--- config/user.php
+++ config/user.php
@@ -2,11 +2,11 @@
 
 return [
 
-    'admin_name' => 'CloudCMR_admin',
-    'admin_email' => 'cloudcmr_admin@gmail.com',
-    'admin_password' => '!!ChangeMe!!',
-    'benevole_name' => 'CloudCMR_benevole',
-    'benevole_email' => 'cloudcmr_benevole@gmail.com',
-    'benevole_password' => '!!ChangeMe!!'
+    'admin_name' => env('USER_ADMIN_NAME', 'CloudCMR_admin'),
+    'admin_email' => env('USER_ADMIN_EMAIL', 'cloudcmr_admin@gmail.com'),
+    'admin_password' => env('USER_ADMIN_PASSWORD', 'secret'),
+    'benevole_name' => env('USER_BENEVOLE_NAME', 'CloudCMR_benevole'),
+    'benevole_email' => env('USER_BENEVOLE_EMAIL', 'cloudcmr_benevole@gmail.com'),
+    'benevole_password' => env('USER_BENEVOLE_PASSWORD', 'secret'),
 
 ];
